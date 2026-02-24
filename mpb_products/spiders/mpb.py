9ded7b88835e491ba7e2e48ca5a84cfc86b3c171
shahmdkhan <shahmuhmdkhan@gmail.com>
2026-02-24 12:54:40 +0500

    Introducing curl_cffi

--- mpb_products/spiders/mpb.py
+++ mpb_products/spiders/mpb.py
@@ -8,7 +8,8 @@ import uuid
 from datetime import datetime
 from collections import defaultdict
 
-from scrapy import Spider, Request
+from curl_cffi import requests
+from scrapy import Spider, Request, Selector
 
 
 class MpbSpider(Spider):
@@ -69,7 +70,8 @@ class MpbSpider(Spider):
         self.notes_filename = 'input/mpb_product_notes.csv'
         self.output_filename = f'output/mpb_products_{datetime.now().strftime("%d%m%Y%H%M")}.json'
         self.failed_pages_status = []
-        self.seen_product_notes_items = {row.get('sku'):row.get('notes') for row in self.read_csv_file() if row.get('sku')}
+        self.seen_product_notes_items = {row.get('sku'): row.get('notes') for row in self.read_csv_file() if
+                                         row.get('sku')}
         self.seen_product_notes_skus = set(self.seen_product_notes_items.keys())
         self.seen_product_urls = []
         self.current_scrapped_items = []
@@ -77,7 +79,7 @@ class MpbSpider(Spider):
         self.failed_pages = 0
 
     def start_requests(self):
-        #working url below
+        # working url below
         url = 'https://www.mpb.com/search-service/product/query/?filter_query[object_type]=product&filter_query[product_condition_star_rating]=%5B1%20TO%205%5D%20AND%20NOT%200&filter_query[model_market]=EU&filter_query[model_available]=true&filter_query[model_is_published_out]=true&field_list=model_name&field_list=model_description&field_list=product_price&field_list=model_url_segment&field_list=product_sku&field_list=product_condition&field_list=product_shutter_count&field_list=product_hour_count&field_list=product_battery_charge_count&field_list=product_id&field_list=product_images&field_list=model_id&field_list=product_price_reduction&field_list=product_price_original&field_list=product_price_modifiers&field_list=model_available_new&sort[product_last_online]=DESC&facet_minimum_count=1&facet_field=model_brand&facet_field=model_category&facet_field=model_product_type&facet_field=product_condition_star_rating&facet_field=product_price&facet_field=%2A&start=0&rows=1000&minimum_match=100%25'
         yield Request(url=url, headers=self.headers,
                       # meta={"playwright": True},
@@ -88,31 +90,34 @@ class MpbSpider(Spider):
                           ],
                       },
                       errback=self.errback_handler,
-                      cookies=self.cookies)
+                      cookies=self.cookies
+                      )
 
     def parse(self, response, **kwargs):
         yield from self.parse_products(response)
 
         # # TODO: Uncomment these below code lines for production
-        # try:
-        #     json_data = json.loads(response.css('pre ::text').get(''))
-        # except:
-        #     json_data = {}
-        #
-        # total_results = json_data.get('total_results') or 0
-        # total_page = ceil(total_results/1000)
-        #
+        try:
+            json_data = json.loads(response.css('pre ::text').get(''))
+        except:
+            json_data = {}
+
+        total_results = json_data.get('total_results') or 0
+        total_page = ceil(total_results / 1000)
+
+        # # TODO: Uncomment these below code lines for production
         # for page_number in range(1,total_page+1):
-        #     next_page_url = response.url.replace('&start=0',f'&start={page_number*1000}')
-        #     yield Request(url=next_page_url, headers=self.headers,
-        #                   # meta={"playwright": True},
-        #                   meta={
-        #                       "playwright": True,
-        #                       "playwright_page_methods": [
-        #                           ("wait_for_load_state", "networkidle"),
-        #                       ],
-        #                   },
-        #                   callback=self.parse_products,errback=self.errback_handler, cookies=self.cookies)
+        for page_number in range(1, 2 + 1):
+            next_page_url = response.url.replace('&start=0', f'&start={page_number * 1000}')
+            yield Request(url=next_page_url, headers=self.headers,
+                          # meta={"playwright": True},
+                          meta={
+                              "playwright": True,
+                              "playwright_page_methods": [
+                                  ("wait_for_load_state", "networkidle"),
+                              ],
+                          },
+                          callback=self.parse_products, errback=self.errback_handler, cookies=self.cookies)
 
     def parse_products(self, response):
         try:
@@ -154,17 +159,25 @@ class MpbSpider(Spider):
                 yield item
                 continue
 
-            yield Request(url=product_url, headers=self.headers,
-                          # meta={"playwright": True},
-                          meta={
-                              "playwright": True,
-                              "playwright_page_methods": [
-                                  ("wait_for_load_state", "networkidle"),
-                              ],
-                          },
-                          callback=self.parse_details,errback=self.errback_handler)
+            yield from self.parse_details(product_url)
+            d = 1
 
-    def parse_details(self, response):
+            # yield Request(url=product_url, headers=self.headers,
+            #               # meta={"playwright": True},
+            #               meta={
+            #                   "playwright": True,
+            #                   "playwright_page_methods": [
+            #                       ("wait_for_load_state", "networkidle"),
+            #                   ],
+            #               },
+            #               callback=self.parse_details,errback=self.errback_handler)
+
+    def parse_details(self, product_url):
+        product_response = requests.get(product_url, headers=self.headers, impersonate="chrome")
+        print(f'\nResponse status:{product_response.status_code} for Product:{product_url}\n')
+
+        response = Selector(text=product_response.text)
+
         try:
             json_data = json.loads(response.css('#__NEXT_DATA__ ::text').get(''))['props']['pageProps']
         except:
@@ -174,19 +187,23 @@ class MpbSpider(Spider):
         product_info = json_data.get('productInfo') or {}
 
         item = dict()
-        item['product_title'] = product_info.get('name') or response.css('.product-name ::text').get('') or model_info.get('brand', {}).get('name')
+        item['product_title'] = product_info.get('name') or response.css('.product-name ::text').get(
+            '') or model_info.get('brand', {}).get('name')
         item['sku'] = product_info.get('sku')
         item['price'] = product_info.get('listPrice')
         item['condition'] = product_info.get('condition')
         item['availability'] = 'in_stock' if not product_info.get('isSold') else 'out_of_stock'
-        item['shutter_count'] = ''.join([attr.get('content') for attr in product_info.get('attributes', []) or [] if attr.get('name', '').lower() == 'SHUTTER_COUNT'.lower()][:1]).strip() or response.css('[data-testid="product-details__shutter-count-attribute__title"] strong ::text').get('')
+        item['shutter_count'] = ''.join([attr.get('content') for attr in product_info.get('attributes', []) or [] if
+                                         attr.get('name', '').lower() == 'SHUTTER_COUNT'.lower()][
+                                        :1]).strip() or response.css(
+            '[data-testid="product-details__shutter-count-attribute__title"] strong ::text').get('')
         item['notes'] = ', '.join([r.get('tierDescription') for r in product_info.get('observations', []) or []])
-        item['url'] = response.url
+        item['url'] = product_url
 
         self.current_scrapped_items.append(item)
 
-        #write input into csv file to reduce request in future
-        self.write_item_into_csv_file(item={'sku':item['sku'], 'notes':item['notes']})
+        # write input into csv file to reduce request in future
+        self.write_item_into_csv_file(item={'sku': item['sku'], 'notes': item['notes']})
 
         yield item
 
@@ -252,7 +269,7 @@ class MpbSpider(Spider):
             "products": products
         }
 
-        #store results for email
+        # store results for email
         self.summary_data = result
 
         # to ensure that all  directories are exists
@@ -261,7 +278,7 @@ class MpbSpider(Spider):
         # Save to JSON file
         with open(self.output_filename, "w", encoding="utf-8") as json_file:
             json.dump(result, json_file, indent=4, ensure_ascii=False)
-    
+
     def get_first_value(self, row, key, default=None):
         # Safely get the dict, then the list, then the first item
         values = row.get(key, {}).get('values', [])
